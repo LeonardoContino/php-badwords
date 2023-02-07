88ad47f1e3fd9674448eb056b450cce53805a073
LeonardoContino <leoconti2000@hotmail.it>
2023-02-07 14:43:55 +0100

    ex done

--- result.php
+++ result.php
@@ -2,4 +2,30 @@
 $badword = $_GET['badword'];
 $paragraph = $_GET['paragraph'];
 
+$censured_paragraph = str_replace($badword,'***', $paragraph);
+
+$paragraph_length = strlen($paragraph);
+$censured_paragraph_length = strlen($censured_paragraph)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h3>testo originale</h3>
+        <p><?= $paragraph ?></p>
+        <p>il testo orginale è lungo <?= $paragraph_length ?> caratteri</p>
+    </section>
+    <section>
+        <h3>testo censurato</h3>
+        <p><?= $censured_paragraph ?></p>
+        <p>il testo orginale è lungo <?= $censured_paragraph_length ?> caratteri</p>
+    </section>
+</body>
+</html>
